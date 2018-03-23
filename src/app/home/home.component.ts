49302a0735147f3577ef242a9838f13403ae4585
Ankit Agarwal <ankitakkii24@gmail.com>
2018-03-23 16:58:32 +0530

    backend hello msg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StoryReaderService } from '../story-reader.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  helloMessage: String;
+  constructor(
+    private storyReaderService: StoryReaderService
+  ) { }
 
   ngOnInit() {
+    this.sayHello();
+  }
+
+  sayHello(): void {
+    this.storyReaderService.sayHello()
+      .subscribe(res => this.helloMessage = res.data);
   }
 
 }
